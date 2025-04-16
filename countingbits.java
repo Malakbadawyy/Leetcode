40021a9e600eead3f22739ad8fc1406d16160736
Malak Badawy <malakbadawy3@gmail.com>
2025-04-17 01:16:27 +0200

    8/75

---
+++ countingbits.java
@@ -0,0 +1,25 @@
+public class countingbits {
+    //bad code, not working
+    // public int[] countBits(int n) {
+    //     int[] answer = int[n];
+    //     int[] base = [0,1,1,2];
+    //     int counter=1;
+    //     int counterref=1;
+    //     int i=0
+    //    while(i<n){
+    //     while(counter!=0){
+    //         for(int j=0; j<base.length;j++){
+                
+
+    //         }
+          
+    //     }
+    //       for(int j=0; j<base.length;j++){
+    //             base[i]+1
+    //         }
+
+    //     }
+
+    // }
+        
+}
