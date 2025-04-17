f9c3ceee281a4fe5d6b4035f433b19373213d9da
Malak Badawy <malakbadawy3@gmail.com>
2025-04-17 14:59:06 +0200

    21. Merge Two Sorted Lists

--- mergeTwoSortedLists.java
+++ mergeTwoSortedLists.java
@@ -52,4 +52,28 @@ public class mergeTwoSortedLists {
 
     }
 
+
+
+    public ListNode mergeTwoListsOpt(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(); // Dummy node to simplify edge cases
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // If there are remaining nodes in either list, attach them
+        current.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next; // The actual merged head
+    }
+
+
 }
